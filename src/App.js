fd33c7e6e93425b44471d9267373a6416d39e1de
Gowtham <gowtham04369g@gmail.com>
2025-03-31 22:53:12 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import Footer from './componets/Footer/Footer';
+import Hero from './componets/Hero/Hero';
+import Join from './componets/Join/Join';
+import Plans from './componets/Plans/Plans';
+import Programs from './componets/Programs/Programs';
+import Reasons from './componets/Reasons/Reasons';
+import Testimonials from './componets/Testimonials/Testimonials';
+
+
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+        <Hero/>
+        <Programs/>
+        <Reasons/>
+        <Plans/>
+        <Testimonials/>
+        <Join/>
+        <Footer/>
+        
     </div>
   );
 }
